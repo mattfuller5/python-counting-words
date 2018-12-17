350f92aaa34686fc499af8daa9f8601a636bf377
Matthew <fullma1906@gmail.com>
2018-12-17 05:45:51 -0800

    first version

---
+++ counting words in file.py
@@ -0,0 +1,12 @@
+file=open("proxy.rtf","r+")
+wordcount={}
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word]=1
+    else:
+        wordcount[word]=wordcount[word]+1
+for item in wordcount.items(): print("{}\t{}".format(*item))
+
+        
+        
+
