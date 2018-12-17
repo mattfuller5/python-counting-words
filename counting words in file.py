0ac3c4d7cf9c28f9e3d5713e7df125ef5609ec69
Matthew <fullma1906@gmail.com>
2018-12-17 06:23:16 -0800

    removing special characters

--- counting words in file.py
+++ counting words in file.py
@@ -1,6 +1,9 @@
-file=open("proxy.rtf","r+")
+special_characters=[".",",",":"]
+file=open("proxy-clean-text.txt","r+")
 wordcount={}
 for word in file.read().split():
+    for special_character in special_characters:
+        word=word.replace(special_character,"")
     if word not in wordcount:
         wordcount[word]=1
     else:
