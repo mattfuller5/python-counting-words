fd807393371ac5422d5ea0c073a474d4d5478000
Matthew <fullma1906@gmail.com>
2018-12-18 06:03:43 -0800

    List in order

--- counting words in file.py
+++ counting words in file.py
@@ -1,24 +1,12 @@
-
-def remove_special_characters(word):
-    new_word=""
-    for character in word:
-        ascii_code=ord(character)
-        if ascii_code>=ord("a") and ascii_code<=ord("z"):
-            new_word=new_word+character
-    return new_word
-
-file_name=input("please enter the file name you would like to be scanned")
-file=open(file_name,"r+")
+file=open("proxy-clean-text.txt","r+")
 wordcount={}
-for word in file.read().lower().split():
-    word=remove_special_characters(word)
+for word in file.read().split():
+    
     if word not in wordcount:
         wordcount[word]=1
     else:
         wordcount[word]=wordcount[word]+1
-for item in wordcount.items(): print("{}:\n{}".format(*item))
-
-
+for item in wordcount.items(): print("{}\t{}".format(*item))
 
         
         
